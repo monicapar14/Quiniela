32573e073a6ed8853c87fd25071ccea717b125bc
monicapar14 <monicapar14@gmail.com>
2026-06-30 12:59:20 -0600

    Update partidosController.ts

--- backend/src/controllers/partidosController.ts
+++ backend/src/controllers/partidosController.ts
@@ -4,18 +4,26 @@ import pool from '../db'
 //obtener toda la informacion de los partidos
 export const getInfoPartidos = async (req: Request, res: Response) => {
   try {
-    const [rows] = await pool.query(`SELECT b.id, a.nombre as grup_nom, f.nombre as fase_nom, b.equipo_local, b.equipo_visitante, 
-                                            b.fecha, b.goles_local, b.goles_visitante, b.fase_id,
-                                            CASE
-									          WHEN b.goles_local = b.goles_visitante
+    const [rows] = await pool.query(`SELECT 
+									    b.id, 
+									    a.nombre AS grup_nom, 
+									    f.nombre AS fase_nom, 
+									    b.equipo_local, 
+									    b.equipo_visitante, 
+									    b.fecha, 
+									    b.goles_local, 
+									    b.goles_visitante, 
+									    b.fase_id,
+									    CASE
+									        WHEN b.goles_local = b.goles_visitante
 									            AND b.fase_id <> 1
-									          THEN b.ganador
-									          ELSE NULL
-									        END AS ganador
-									      FROM partidos b
-									      JOIN grupos g ON g.id = b.grupo_id
-									      JOIN fases f ON f.id = b.fase_id
-									      ORDER BY b.fase_id ASC, b.fecha ASC`)
+									        THEN b.ganador
+									        ELSE NULL
+									    END AS ganador
+									FROM partidos b
+									JOIN fases f ON f.id = b.fase_id
+									LEFT JOIN grupos a ON a.id = b.grupo_id
+									ORDER BY b.fase_id ASC, b.fecha ASC`)
     res.json(rows)
   } catch (error) {
     console.error('Error al obtener los productos:', error)
